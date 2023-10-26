ec7d9ca3298de891f7a00a4257d531ec79891cfd
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-10-26 10:53:05 -0500

    workflow class

--- core/src/querent/workflow/workflow.rs
+++ core/src/querent/workflow/workflow.rs
@@ -6,31 +6,38 @@ use std::{collections::HashMap, sync::Mutex};
 pub struct Workflow {
 	pub name: String,
 	pub id: String,
+	pub python_import_path: String,
 	pub config: Config,
 }
 
-#[derive(Debug)]
 pub struct WorkflowManager {
 	workflows: Mutex<HashMap<String, Workflow>>,
+	running_workflows: Mutex<HashMap<String, Workflow>>,
 }
 
 impl WorkflowManager {
 	pub fn new() -> Self {
-		WorkflowManager { workflows: Mutex::new(HashMap::new()) }
+		WorkflowManager {
+			workflows: Mutex::new(HashMap::new()),
+			running_workflows: Mutex::new(HashMap::new()),
+		}
 	}
 
 	pub fn add_workflow(&self, workflow: Workflow) -> Result<(), String> {
 		let mut workflows = self.workflows.lock().map_err(|_| "Mutex lock failed.".to_string())?;
 		if workflows.contains_key(&workflow.id) {
-			Err("Workflow with the same ID already exists.".to_string())
+			return Err("Workflow with the same ID already exists.".to_string())
 		} else {
-			workflows.insert(workflow.id.clone(), workflow);
-			Ok(())
+			workflows.insert(workflow.id.clone(), workflow.clone());
 		}
+		Ok(())
 	}
 
 	pub fn remove_workflow(&self, id: &str) -> Result<(), String> {
 		let mut workflows = self.workflows.lock().map_err(|_| "Mutex lock failed.".to_string())?;
+		if self.running_workflows.lock().unwrap().contains_key(id) {
+			return Err("Workflow is running.".to_string())
+		}
 		if workflows.remove(id).is_some() {
 			Ok(())
 		} else {
@@ -48,30 +55,80 @@ impl WorkflowManager {
 		workflows.values().cloned().collect()
 	}
 
-	pub fn start_workflow(&self, id: &str) -> Result<(), String> {
+	pub fn is_running(&self, id: &str) -> bool {
+		let running_workflows = self.running_workflows.lock().unwrap();
+		running_workflows.contains_key(id)
+	}
+}
+
+impl WorkflowManager {
+	pub async fn start_workflow(&self, id: &str) -> Result<(), String> {
 		let workflow = self.get_workflow(id).ok_or("Workflow not found.")?;
-		Python::with_gil(|py| {
-			// Implement Python interaction with the workflow here
-			// For example: let workflow_config = &workflow.config;
-			Ok(())
+		if self.is_running(id) {
+			return Err("Workflow is already running.".to_string())
+		}
+
+		let config = workflow.config.clone();
+
+		let workflow_future = Python::with_gil(|py| {
+			let querent_start_workflow = py
+				.import(workflow.python_import_path.as_str())
+				.map_err(|_| "Failed to import workflow.")?;
+			let inner_fut = pyo3_asyncio::async_std::into_future(
+				querent_start_workflow
+					.call_method1("start", (config,))
+					.map_err(|_| "Failed to start workflow.")?,
+			);
+			if let Ok(fut) = inner_fut {
+				Ok(fut)
+			} else {
+				Err("Failed to start workflow.".to_string())
+			}
 		})
+		.map_err(|_| "Failed to start workflow.".to_string());
+
+		let workflow_future = workflow_future?;
+
+		match workflow_future.await {
+			Ok(_) => {
+				let mut running_workflows = self.running_workflows.lock().unwrap();
+				running_workflows.insert(id.to_string(), workflow);
+			},
+			Err(_) => return Err("Failed to start workflow.".to_string()),
+		}
+
+		Ok(())
 	}
 
-	pub fn kill_workflow(&self, id: &str) -> Result<(), String> {
+	pub async fn stop_workflow(&self, id: &str) -> Result<(), String> {
 		let workflow = self.get_workflow(id).ok_or("Workflow not found.")?;
-		Python::with_gil(|py| {
-			// Implement Python interaction with the workflow here
-			// For example: let workflow_config = &workflow.config;
-			Ok(())
-		})
-	}
+		if !self.is_running(id) {
+			return Err("Workflow is not running.".to_string())
+		}
 
-	pub fn restart_workflow(&self, id: &str) -> Result<(), String> {
-		let workflow = self.get_workflow(id).ok_or("Workflow not found.")?;
-		Python::with_gil(|py| {
-			// Implement Python interaction with the workflow here
-			// For example: let workflow_config = &workflow.config;
-			Ok(())
-		})
+		let workflow_future = Python::with_gil(|py| {
+			let querent_stop_workflow = py
+				.import(workflow.python_import_path.as_str())
+				.map_err(|_| "Failed to import workflow.")?;
+			let inner_fut = pyo3_asyncio::async_std::into_future(
+				querent_stop_workflow
+					.call_method0("stop")
+					.map_err(|_| "Failed to stop workflow.")?,
+			);
+			if let Ok(fut) = inner_fut {
+				Ok(fut)
+			} else {
+				Err("Failed to stop workflow.".to_string())
+			}
+		});
+		match workflow_future {
+			Ok(fut) => {
+				let mut running_workflows = self.running_workflows.lock().unwrap();
+				running_workflows.remove(id);
+				fut.await.map_err(|_| "Failed to stop workflow.".to_string())?;
+				Ok(())
+			},
+			Err(_) => return Err("Failed to stop workflow.".to_string()),
+		}
 	}
 }
