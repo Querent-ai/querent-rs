1cc86806955f80d4c8e67a05964e3695b36c4385
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-10-29 20:39:09 -0500

    set a test

--- core/src/querent/workflow/workflow.rs
+++ core/src/querent/workflow/workflow.rs
@@ -1,7 +1,7 @@
 use crate::config::Config;
 use pyo3::prelude::*;
 use std::{collections::HashMap, sync::Mutex};
-use tokio::{sync::mpsc, task::spawn_blocking};
+use tokio::task::spawn_blocking;
 
 /// Represents a workflow.
 #[derive(Debug, Clone)]
@@ -17,16 +17,12 @@ pub struct Workflow {
 /// Manages workflows and their execution.
 pub struct WorkflowManager {
 	workflows: Mutex<HashMap<String, Workflow>>,
-	running_workflows: Mutex<HashMap<String, tokio::task::JoinHandle<Result<(), String>>>>,
 }
 
 impl WorkflowManager {
 	/// Creates a new `WorkflowManager` instance.
 	pub fn new() -> Self {
-		WorkflowManager {
-			workflows: Mutex::new(HashMap::new()),
-			running_workflows: Mutex::new(HashMap::new()),
-		}
+		WorkflowManager { workflows: Mutex::new(HashMap::new()) }
 	}
 
 	/// Adds a workflow to the manager.
@@ -40,47 +36,15 @@ impl WorkflowManager {
 	/// Returns a `Result` indicating success or an error message.
 	pub fn add_workflow(&self, workflow: Workflow) -> Result<(), String> {
 		let mut workflows = self.workflows.lock().map_err(|_| "Mutex lock failed.".to_string())?;
+		if workflows.len() >= 1 {
+			return Err("Only one workflow is supported.".to_string())
+		}
 		if workflows.contains_key(&workflow.id) {
 			return Err("Workflow with the same ID already exists.".to_string())
 		} else {
 			workflows.insert(workflow.id.clone(), workflow.clone());
 		}
 		Ok(())
-	}
-
-	/// Removes a workflow from the manager.
-	///
-	/// # Arguments
-	///
-	/// * `id` - The ID of the workflow to be removed.
-	///
-	/// # Returns
-	///
-	/// Returns a `Result` indicating success or an error message.
-	pub fn remove_workflow(&self, id: &str) -> Result<(), String> {
-		let mut workflows = self.workflows.lock().map_err(|_| "Mutex lock failed.".to_string())?;
-		if self.running_workflows.lock().unwrap().contains_key(id) {
-			return Err("Workflow is running.".to_string())
-		}
-		if workflows.remove(id).is_some() {
-			Ok(())
-		} else {
-			Err("Workflow not found.".to_string())
-		}
-	}
-
-	/// Retrieves a workflow by ID.
-	///
-	/// # Arguments
-	///
-	/// * `id` - The ID of the workflow to be retrieved.
-	///
-	/// # Returns
-	///
-	/// Returns an `Option` containing the retrieved workflow or `None`.
-	pub fn get_workflow(&self, id: &str) -> Option<Workflow> {
-		let workflows = self.workflows.lock().ok()?;
-		workflows.get(id).cloned()
 	}
 
 	/// Retrieves a list of all workflows managed by this manager.
@@ -93,118 +57,94 @@ impl WorkflowManager {
 		workflows.values().cloned().collect()
 	}
 
-	/// Checks if a workflow with the given ID is currently running.
-	///
-	/// # Arguments
-	///
-	/// * `id` - The ID of the workflow to be checked.
-	///
-	/// # Returns
-	///
-	/// Returns `true` if the workflow is running; otherwise, returns `false`.
-	pub fn is_running(&self, id: &str) -> bool {
-		let running_workflows = self.running_workflows.lock().unwrap();
-		running_workflows.contains_key(id)
-	}
-
 	/// Starts a workflow by executing its Python code asynchronously.
-	///
-	/// # Arguments
-	///
-	/// * `id` - The ID of the workflow to be started.
-	/// * `tx` - A channel sender for status messages.
 	///
 	/// # Returns
 	///
 	/// Returns a `Result` indicating success or an error message.
-	pub async fn start_workflow(&self, id: String, tx: mpsc::Sender<String>) -> Result<(), String> {
-		let workflow = self.get_workflow(&id).ok_or("Workflow not found.")?;
-		if self.is_running(&id) {
-			return Err("Workflow is already running.".to_string())
-		}
+	pub async fn start_workflows(&self) -> Result<(), String> {
 		pyo3::prepare_freethreaded_python();
+		let workflows = self.get_workflows();
+		let handles: Vec<_> = workflows
+			.iter()
+			.map(|workflow| {
+				let config = workflow.config.clone();
+				let python_import_path = workflow.python_import_path.clone();
+				let python_start_function = workflow.python_start_function.clone();
+				spawn_blocking(move || {
+					Python::with_gil(|py| {
+						let querent_start_workflow = py
+							.import(python_import_path.as_str())
+							.map_err(|_| "Failed to import workflow.");
+						if querent_start_workflow.is_err() {
+							panic!("Failed to import workflow.")
+						}
+						let coroutine = querent_start_workflow
+							.unwrap()
+							.call_method1(python_start_function.as_str(), (config,))
+							.map_err(|_| "Failed to start workflow.")?;
 
-		let config = workflow.config.clone();
-		let python_import_path = workflow.python_import_path.clone();
-		let python_start_function = workflow.python_start_function.clone();
-		let id_clone = id.clone();
-		let handle = spawn_blocking(move || {
-			Python::with_gil(|py| {
-				println!("Starting workflow {}...", id_clone.clone());
-				let querent_start_workflow = py
-					.import(python_import_path.as_str())
-					.map_err(|_| "Failed to import workflow.");
-				println!("querent_start_workflow: {:?}", querent_start_workflow);
-				let result = querent_start_workflow
-					.unwrap()
-					.call_method1(python_start_function.as_str(), (config,))
-					.map_err(|_| "Failed to start workflow.");
-				println!("result: {:?}", result);
-				match result {
-					Ok(_) => {
-						let _ = tx.send(format!("Workflow {} has started.", id_clone.clone()));
-						Ok(())
-					},
-					Err(_) => {
-						let _ = tx.send(format!("Failed to start Workflow {}.", id_clone.clone()));
-						Err("Failed to start workflow.".to_string())
-					},
-				}
+						pyo3_asyncio::tokio::into_future(coroutine)
+							.map_err(|_| "Failed to start workflow.")
+					})
+				})
 			})
-		});
+			.collect();
 
-		let mut running_workflows = self.running_workflows.lock().unwrap();
-		running_workflows.insert(id, handle);
+		for handle in handles {
+			let result = handle.await.map_err(|_| "Failed to start workflow.".to_string())?;
+			if result.is_err() {
+				return Err("Failed to start workflow.".to_string())
+			}
 
+			let result = result.unwrap();
+			match result.await {
+				Ok(_) => {
+					println!("Workflow started.");
+					println!("Waiting for workflow to complete...");
+				},
+				Err(_) => return Err("Failed to start workflow.".to_string()),
+			}
+		}
 		Ok(())
 	}
 
 	/// Stops a running workflow by executing its Python code asynchronously.
 	///
-	/// # Arguments
-	///
-	/// * `id` - The ID of the workflow to be stopped.
-	/// * `tx` - A channel sender for status messages.
-	///
 	/// # Returns
 	///
 	/// Returns a `Result` indicating success or an error message.
-	pub async fn stop_workflow(&self, id: String, tx: mpsc::Sender<String>) -> Result<(), String> {
-		let workflow = self.get_workflow(&id).ok_or("Workflow not found.")?;
-		if !self.is_running(&id) {
-			return Err("Workflow is not running.".to_string())
-		}
+	pub async fn stop_workflows(&self) -> Result<(), String> {
+		pyo3::prepare_freethreaded_python();
+		let workflows = self.get_workflows();
+		let handles: Vec<_> = workflows
+			.iter()
+			.map(|workflow| {
+				let python_import_path = workflow.python_import_path.clone();
+				let python_stop_function = workflow.python_stop_function.clone();
+				spawn_blocking(move || {
+					Python::with_gil(|py| {
+						let querent_stop_workflow = py
+							.import(python_import_path.as_str())
+							.map_err(|_| "Failed to import workflow.");
+						let coroutine = querent_stop_workflow
+							.unwrap()
+							.call_method0(python_stop_function.as_str())
+							.map_err(|_| "Failed to stop workflow.");
 
-		let handle = self.running_workflows.lock().unwrap().remove(&id).unwrap();
-		let _result = handle.await.map_err(|_| "Failed to stop workflow.".to_string())?;
-
-		let python_import_path = workflow.python_import_path.clone();
-		let id_clone = id.clone();
-		let python_stop_function = workflow.python_stop_function.clone();
-		let result = spawn_blocking(move || {
-			Python::with_gil(|py| {
-				let querent_stop_workflow = py
-					.import(python_import_path.as_str())
-					.map_err(|_| "Failed to import workflow.")?;
-				let result = querent_stop_workflow
-					.call_method0(python_stop_function.as_str())
-					.map_err(|_| "Failed to stop workflow.");
-
-				match result {
-					Ok(_) => {
-						let _ = tx.send(format!("Workflow {} has stopped.", id_clone));
-						Ok(())
-					},
-					Err(_) => {
-						let _ = tx.send(format!("Failed to stop Workflow {}.", id_clone));
-						Err("Failed to stop workflow.".to_string())
-					},
-				}
+						pyo3_asyncio::tokio::into_future(coroutine.unwrap())
+							.map_err(|_| "Failed to stop workflow.")
+					})
+				})
 			})
-		})
-		.await
-		.map_err(|_| "Failed to stop workflow.".to_string())?;
+			.collect();
 
-		result
+		for handle in handles {
+			let result = handle.await.map_err(|_| "Failed to stop workflow.".to_string())?;
+			if result.is_err() {
+				return Err("Failed to stop workflow.".to_string())
+			}
+		}
+		Ok(())
 	}
 }
--- core/src/tests/mod.rs
+++ core/src/tests/mod.rs
@@ -1,2 +1,1 @@
 mod test_callback_interface;
-mod test_workflow_manager;
