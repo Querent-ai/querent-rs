b6e9eca0c8fdc57df03666e41b8212d99b0f25e7
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-11-10 05:52:24 -0600

    test code path, incremental update

--- pytests/test_workflow_manager.rs
+++ pytests/test_workflow_manager.rs
@@ -34,3 +34,36 @@ async fn workflow_manager_basic_tests() -> pyo3::PyResult<()> {
 	}
 	Ok(())
 }
+
+const CODE: &str = r#"
+def function(*args):
+    assert args == ("hello",)
+    return "called with args and kwargs"
+"#;
+
+#[pyo3_asyncio::tokio::test]
+async fn workflow_manager_with_code_tests() -> pyo3::PyResult<()> {
+	let workflow_manager = WorkflowManager::new();
+
+	let mut args: Vec<CLRepr> = Vec::new();
+	args.push(CLRepr::String("Hello".to_string(), StringType::Normal));
+
+	let test_flow_with_code: Workflow = Workflow {
+		name: "test_flow_with_code".to_string(),
+		id: "id2".to_string(),
+		import: "asyncio".to_string(),
+		attr: "function".to_string(),
+		arguments: args,
+		code: Some(CODE.to_string()),
+	};
+
+	assert!(workflow_manager.add_workflow(test_flow_with_code).is_ok());
+
+	match workflow_manager.start_workflows().await {
+		Ok(_) => {
+			assert!(true);
+		},
+		Err(e) => panic!("Error starting workflows: {}", e),
+	}
+	Ok(())
+}
