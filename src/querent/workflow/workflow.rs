29fe8a7e210155f12c621d9ca2ab1f7c06a25a12
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-11-11 05:05:19 -0600

    use tokio over async std, set tokio pyo3

--- src/querent/workflow/workflow.rs
+++ src/querent/workflow/workflow.rs
@@ -78,71 +78,22 @@ impl WorkflowManager {
 	/// Returns a `Result` indicating success or an error message.
 	pub async fn start_workflows(&self) -> Result<(), QuerentError> {
 		let workflows = self.get_workflows();
-		let handles: Vec<_> = workflows
-			.iter()
-			.map(|workflow| {
-				let args = workflow.arguments.clone();
-				let python_start_function = workflow.attr.clone();
-				let runtime_local = self.runtime;
-				let tokio = tokio_runtime()
-					.map_err(|_| "Failed to init tokio runtime.")
-					.expect("Failed to init tokio runtime.");
 
-				let call_future = Python::with_gil(|py| {
-					if let Some(code) = &workflow.code {
-						let querent_code_based = PyModule::from_code(
-							py,
-							code.as_str(),
-							&format!("{}.py", workflow.name),
-							&workflow.name,
-						)
-						.map_err(|_| "Failed to create dynamic module.")
-						.expect("Failed to create dynamic module.");
+		for _workflow in workflows {
+			let res = Python::with_gil(|py| {
+				let asyncio = py.import("asyncio")?;
 
-						let querent_start_workflow = querent_code_based
-							.getattr(python_start_function.as_str())
-							.map_err(|_| "Failed to find start function.")
-							.expect("Failed to find start function.");
+				// Convert asyncio.sleep into a Rust Future
+				let sleep_future = pyo3_asyncio::tokio::into_future(
+					asyncio.call_method1("sleep", (1.into_py(py),))?,
+				);
 
-						let querent_py_fun: Py<PyFunction> =
-							querent_start_workflow.extract().expect("Failed to extract function.");
-
-						runtime_local.call_async(querent_py_fun, args)
-					} else {
-						let python_import_path = workflow.import.clone();
-
-						let querent_start_workflow = py
-							.import(python_import_path.as_str())
-							.map_err(|_| "Failed to import workflow.")
-							.expect("Failed to import workflow.");
-
-						let coroutine = querent_start_workflow
-							.getattr(python_start_function.as_str())
-							.map_err(|_| "Failed to find start function.")
-							.expect("Failed to find start function.");
-
-						let querent_py_fun: Py<PyFunction> =
-							coroutine.extract().expect("Failed to extract function");
-
-						runtime_local.call_async(querent_py_fun, args)
-					}
-				});
-
-				tokio.spawn(call_future)
+				sleep_future
 			})
-			.collect();
+			.map_err(|e| QuerentError::internal(e.to_string()))?;
 
-		for handle in handles {
-			let result = handle
-				.await
-				.map(|_| log::info!("Workflow started successfully."))
-				.map_err(|_| QuerentError::internal("Error starting workflow.".to_string()));
-
-			if let Err(e) = result {
-				panic!("Error starting workflow: {}", e);
-			}
+			res.await.map_err(|e| QuerentError::internal(e.to_string()))?;
 		}
-
 		Ok(())
 	}
 }
