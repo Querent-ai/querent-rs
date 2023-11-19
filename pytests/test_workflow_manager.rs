df71f56e962823cdb4ac4fd058d62a0f8e6dd6f5
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-11-19 07:51:17 +0530

    dynamic code test, python to rust future with tokio

--- pytests/test_workflow_manager.rs
+++ pytests/test_workflow_manager.rs
@@ -1,5 +1,5 @@
 use querent_rs::{
-	cross::CLRepr,
+	cross::{CLRepr, StringType},
 	querent::workflow::{Workflow, WorkflowManager},
 };
 
@@ -30,6 +30,45 @@ async fn workflow_manager_basic_tests() -> pyo3::PyResult<()> {
 		attr: "sleep".to_string(),
 		arguments: args,
 		code: None,
+	};
+	assert!(workflow_manager.add_workflow(test_flow).is_ok());
+	match workflow_manager.start_workflows().await {
+		Ok(_) => assert!(true),
+		Err(e) => panic!("Error starting workflows: {}", e),
+	}
+	Ok(())
+}
+
+const _CODE: &str = r#"
+def print_querent(text: str) -> str:
+    """Attempt to parse and execute input as Python"""
+    try:
+        output = eval(text, globals(), globals())
+    except SyntaxError as error:
+        try:
+            output = exec(compile(text, "<string>", "exec"), globals(), globals())
+        except Exception as exception:
+            output = f"{exception=}"
+    return str(output)
+                   "#;
+
+#[pyo3_asyncio::tokio::test]
+async fn workflow_manager_python_tests() -> pyo3::PyResult<()> {
+	let workflow_manager_res = WorkflowManager::new();
+	if let Err(e) = workflow_manager_res {
+		panic!("Error creating workflow manager: {}", e);
+	}
+	let workflow_manager = workflow_manager_res.unwrap();
+	//let config = Config::default();
+	let mut args: Vec<CLRepr> = Vec::new();
+	args.push(CLRepr::String("Querent".to_string(), StringType::Normal));
+	let test_flow: Workflow = Workflow {
+		name: "test_flow_python".to_string(),
+		id: "id2".to_string(),
+		import: "".to_string(),
+		attr: "print_querent".to_string(),
+		arguments: args,
+		code: Some(_CODE.to_string()),
 	};
 	assert!(workflow_manager.add_workflow(test_flow).is_ok());
 	match workflow_manager.start_workflows().await {
