4a6d2c8b192fd6a9d6b719cdbb7b42b322431c35
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-01-14 15:28:03 -0600

    ingested token dict to be sent

--- src/comm/types/ingested_tokens.rs
+++ src/comm/types/ingested_tokens.rs
@@ -1,12 +1,22 @@
-use pyo3::{prelude::*, types::PyDict, PyObject, Python, ToPyObject};
+use pyo3::{prelude::*, types::PyDict, PyObject, Python};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize, utoipa::ToSchema)]
-#[pyclass]
 pub struct IngestedTokens {
 	pub data: Option<Vec<String>>,
 	pub file: String,
 	pub is_token_stream: Option<bool>,
+}
+
+impl IntoPy<PyObject> for IngestedTokens {
+	fn into_py(self, py: Python) -> PyObject {
+		let token_dict = PyDict::new(py);
+		token_dict.set_item("data", &self.data).unwrap();
+		token_dict.set_item("file", &self.file).unwrap();
+		token_dict.set_item("is_token_stream", &self.is_token_stream).unwrap();
+
+		token_dict.into()
+	}
 }
 
 impl ToPyObject for IngestedTokens {
@@ -16,6 +26,6 @@ impl ToPyObject for IngestedTokens {
 		token_dict.set_item("file", &self.file).unwrap();
 		token_dict.set_item("is_token_stream", &self.is_token_stream).unwrap();
 
-		token_dict.to_object(py)
+		token_dict.into()
 	}
 }
