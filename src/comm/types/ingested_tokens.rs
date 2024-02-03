283bcd0218c675952446f56b632bd7a50f3741c3
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-02-03 15:30:01 -0600

    release py compatible

--- src/comm/types/ingested_tokens.rs
+++ src/comm/types/ingested_tokens.rs
@@ -2,11 +2,31 @@ use pyo3::{prelude::*, types::PyDict, PyObject, Python};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize, utoipa::ToSchema)]
-#[pyclass]
 pub struct IngestedTokens {
 	pub data: Option<Vec<String>>,
 	pub file: String,
 	pub is_token_stream: Option<bool>,
+}
+
+impl IntoPy<PyObject> for IngestedTokens {
+	fn into_py(self, py: Python) -> PyObject {
+		let token_dict = PyDict::new(py);
+		token_dict.set_item("data", &self.data).unwrap();
+		token_dict.set_item("file", &self.file).unwrap();
+		token_dict.set_item("is_token_stream", &self.is_token_stream).unwrap();
+
+		token_dict.into()
+	}
+}
+
+impl FromPyObject<'_> for IngestedTokens {
+	fn extract(ob: &PyAny) -> PyResult<Self> {
+		let data = ob.get_item("data")?.extract()?;
+		let file = ob.get_item("file")?.extract()?;
+		let is_token_stream = ob.get_item("is_token_stream")?.extract()?;
+
+		Ok(IngestedTokens { data, file, is_token_stream })
+	}
 }
 
 impl ToPyObject for IngestedTokens {
