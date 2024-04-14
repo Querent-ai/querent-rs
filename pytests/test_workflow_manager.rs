a67b1052b9224e7088eac8ec017ed49aff26f181
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2024-04-14 08:45:56 -0500

    fix tests

--- pytests/test_workflow_manager.rs
+++ pytests/test_workflow_manager.rs
@@ -369,6 +369,7 @@ async def print_querent(config, text: str):
             "timestamp": 123.45,  # Replace with the actual timestamp
             "payload": "🚀🚀",  # Replace with the actual payload data
 			"file": "test_file"  # Replace with the actual file name
+			"doc_source": "test_doc_source"  # Replace with the actual doc source
         }
         config['workflow']['event_handler'].handle_event(event_type, event_data)
 "#;
